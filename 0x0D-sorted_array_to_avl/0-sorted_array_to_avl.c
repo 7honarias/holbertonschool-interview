0e6931e4fffdf6b93dc94bcae3854f70de28869e
Jhon Arias <jhon.arias@mercadolibre.com.co>
2021-10-30 14:24:36 -0500

    ADD - correction

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,99 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
 
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
- * print_t - Stores recursively each level in an array of strings
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
  *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
+ * Return: New Node or NULL if fail
  */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	char b[6];
-	int width, left, right, is_left, i;
+	binary_tree_t *new_node = NULL;
 
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
+}
+/**
+ * sort_insert - Auxiliary Function for sort insertion
+ * @array: pointer to the first element of the array to be converted
+ * @min: lower limit
+ * @max: upper limit
+ *
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sort_insert(int *array, int min, int max)
+{
+	int half;
+	avl_t *tree;
+
+	if (min > max)
+		return (NULL);
+
+	half = (max + min) / 2;
+
+	tree = binary_tree_node(NULL, array[half]);
 	if (!tree)
-		return (0);
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = print_t(tree->left, offset, depth + 1, s);
-	right = print_t(tree->right, offset + left + width, depth + 1, s);
-	for (i = 0; i < width; i++)
-		s[depth][offset + left + i] = b[i];
-	if (depth && is_left)
-	{
-		for (i = 0; i < width + right; i++)
-			s[depth - 1][offset + left + width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	else if (depth && !is_left)
-	{
-		for (i = 0; i < left + width; i++)
-			s[depth - 1][offset - width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	return (left + width + right);
+		return (NULL);
+
+	tree->left = sort_insert(array, min, half - 1);
+
+	tree->right = sort_insert(array, half + 1, max);
+
+	if (tree->left)
+		tree->left->parent = tree;
+
+	if (tree->right)
+		tree->right->parent = tree;
+
+	return (tree);
 }
 
 /**
- * _height - Measures the height of a binary tree
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
  *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
+ * Return: pointer to the root node of the created AVL tree, or NULL on failure
  */
-static size_t _height(const binary_tree_t *tree)
+avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	size_t height_l;
-	size_t height_r;
+	avl_t *root = NULL;
 
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
+	if (!array || size < 1)
+		return (NULL);
 
-/**
- * binary_tree_print - Prints a binary tree
- *
- * @tree: Pointer to the root node of the tree to print
- */
-void binary_tree_print(const binary_tree_t *tree)
-{
-	char **s;
-	size_t height, i, j;
+	root = sort_insert(array, 0, size - 1);
 
-	if (!tree)
-		return;
-	height = _height(tree);
-	s = malloc(sizeof(*s) * (height + 1));
-	if (!s)
-		return;
-	for (i = 0; i < height + 1; i++)
-	{
-		s[i] = malloc(sizeof(**s) * 255);
-		if (!s[i])
-			return;
-		memset(s[i], 32, 255);
-	}
-	print_t(tree, 0, 0, s);
-	for (i = 0; i < height + 1; i++)
-	{
-		for (j = 254; j > 1; --j)
-		{
-			if (s[i][j] != ' ')
-				break;
-			s[i][j] = '\0';
-		}
-		printf("%s\n", s[i]);
-		free(s[i]);
-	}
-	free(s);
+	return (root);
 }
